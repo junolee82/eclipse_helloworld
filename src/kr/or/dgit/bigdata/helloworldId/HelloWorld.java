d29b8c927ffc786eb89389548559d78f521b5695
junolee <joolia82@gmail.com>
2016-12-13 14:51:06 +0900

    HelloWorld.java 한 줄 추가

--- src/kr/or/dgit/bigdata/helloworldId/HelloWorld.java
+++ src/kr/or/dgit/bigdata/helloworldId/HelloWorld.java
@@ -2,6 +2,7 @@ package kr.or.dgit.bigdata.helloworldId;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println("Hello World");
+		System.out.println("Work in Hotfix");
 	}
 }
