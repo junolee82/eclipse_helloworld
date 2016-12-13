2dc03dc2316d5657c240f981e77c261982dac206
junolee <joolia82@gmail.com>
2016-12-13 14:54:49 +0900

    의도적으로 한줄 추가하여 충돌발생

--- src/kr/or/dgit/bigdata/helloworldId/HelloWorld.java
+++ src/kr/or/dgit/bigdata/helloworldId/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Work in Hotfix");
+		System.out.println("의도적으로 충돌작업");
 	}
 }
